c82a7e9616aaee19cf7c768db508c1501745c427
KimYoungWoo <rladuddn3924@gmail.com>
2025-02-04 01:43:40 +0900

    ✅ fetchHolidays 통과 테스트 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,23 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2024-12'));
+    expect(holidays).toEqual({
+      '2024-12-25': '크리스마스',
+    });
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2024-11'));
+    expect(holidays).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2024-10'));
+    expect(holidays).toEqual({
+      '2024-10-03': '개천절',
+      '2024-10-09': '한글날',
+    });
+  });
 });
