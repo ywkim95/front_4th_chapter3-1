bda749f7b58c1ca1ae198de307ca546d90f50647
KimYoungWoo <rladuddn3924@gmail.com>
2025-02-04 08:01:51 +0900

    ✅ notificationUtils 통과 테스트 작성

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -2,15 +2,64 @@ import { Event } from '../../types';
 import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  const events: Event[] = [
+    {
+      id: '1',
+      title: '이벤트 1',
+      date: '2024-07-01',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: 'event 1 description',
+      location: 'event 1 location',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+  ];
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    const now = new Date('2024-07-01T08:50');
+    const notifiedEvents: string[] = [];
+    const upcomingEvents = getUpcomingEvents(events, now, notifiedEvents);
+    expect(upcomingEvents).toEqual([events[0]]);
+  });
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    const now = new Date('2024-07-01T08:50');
+    const notifiedEvents: string[] = ['1'];
+    const upcomingEvents = getUpcomingEvents(events, now, notifiedEvents);
+    expect(upcomingEvents).toEqual([]);
+  });
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    const now = new Date('2024-07-01T08:40');
+    const notifiedEvents: string[] = [];
+    const upcomingEvents = getUpcomingEvents(events, now, notifiedEvents);
+    expect(upcomingEvents).toEqual([]);
+  });
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    const now = new Date('2024-07-01T09:10');
+    const notifiedEvents: string[] = [];
+    const upcomingEvents = getUpcomingEvents(events, now, notifiedEvents);
+    expect(upcomingEvents).toEqual([]);
+  });
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const event: Event = {
+      id: '1',
+      title: '이벤트 1',
+      date: '2024-07-01',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: 'event 1 description',
+      location: 'event 1 location',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    };
+    const message = createNotificationMessage(event);
+    expect(message).toBe('10분 후 이벤트 1 일정이 시작됩니다.');
+  });
 });
