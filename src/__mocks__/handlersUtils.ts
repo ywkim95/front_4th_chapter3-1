bc7ec0b0a51bc56e687e322534e0f0234ee60bd7
KimYoungWoo <rladuddn3924@gmail.com>
2025-02-04 23:20:27 +0900

    ✅ handlers 로직 업데이트
    - 같이 사용하는 handlersUtils의 함수들을 개별로 분리 적용

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -7,54 +7,58 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! 이벤트는 생성, 수정 되면 fetch를 다시 해 상태를 업데이트 합니다. 이를 위한 제어가 필요할 것 같은데요. 어떻게 작성해야 테스트가 병렬로 돌아도 안정적이게 동작할까요?
 // ! 아래 이름을 사용하지 않아도 되니, 독립적이게 테스트를 구동할 수 있는 방법을 찾아보세요. 그리고 이 로직을 PR에 설명해주세요.
 export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
-  const mockEvents = [...initEvents];
+  const mockEvents = structuredClone(initEvents) as Event[];
 
-  server.use(getEvents(mockEvents), createEvent(mockEvents));
+  server.use(
+    http.get('/api/events', () => HttpResponse.json({ events: mockEvents })),
+    http.post('/api/events', async ({ request }) => {
+      const form = (await request.json()) as EventForm;
+      const newEvent = {
+        id: String(mockEvents.length + 1),
+        ...form,
+      };
+      mockEvents.push(newEvent);
+      return HttpResponse.json(newEvent, { status: 201 });
+    }),
+  );
 };
 
 export const setupMockHandlerUpdating = () => {
-  const mockEvents = [...events] as Event[];
+  const mockEvents = structuredClone(events) as Event[];
 
-  server.use(getEvents(mockEvents), updateEvent(mockEvents));
+  server.use(
+    http.get('/api/events', () => HttpResponse.json({ events: mockEvents })),
+    http.put('/api/events/:id', async ({ params, request }) => {
+      const { id } = params;
+      const form = (await request.json()) as Event;
+      const event = mockEvents.find((event) => event.id === id);
+      if (!event) {
+        return HttpResponse.json('Event not found', { status: 404 });
+      }
+      mockEvents.splice(
+        mockEvents.findIndex((event) => event.id === id),
+        1,
+        { ...event, ...form },
+      );
+      return HttpResponse.json({ ...event, ...form }, { status: 200 });
+    }),
+  );
 };
 
 export const setupMockHandlerDeletion = () => {
-  const mockEvents = [...events] as Event[];
+  const mockEvents = structuredClone(events) as Event[];
 
-  server.use(getEvents(mockEvents), deleteEvent(mockEvents));
+  server.use(
+    http.get('/api/events', () => HttpResponse.json({ events: mockEvents })),
+    http.delete('/api/events/:id', ({ params }) => {
+      const { id } = params;
+      const eventIndex = mockEvents.findIndex((event) => event.id === id);
+
+      if (eventIndex === -1) {
+        return HttpResponse.json('Event not found', { status: 404 });
+      }
+      mockEvents.splice(eventIndex, 1);
+      return new HttpResponse(null, { status: 204 });
+    }),
+  );
 };
-
-export const getEvents = (events: Event[]) =>
-  http.get('/api/events', () => HttpResponse.json({ events }));
-
-export const createEvent = (events: Event[]) =>
-  http.post('/api/events', async ({ request }) => {
-    const form = (await request.json()) as EventForm;
-    const newEvent = {
-      ...form,
-      id: String(events.length + 1),
-    };
-    return HttpResponse.json(newEvent, { status: 201 });
-  });
-
-export const updateEvent = (events: Event[]) =>
-  http.put('/api/events/:id', async ({ params, request }) => {
-    const { id } = params;
-    const form = (await request.json()) as Event;
-    const event = events.find((event) => event.id === id);
-    if (!event) {
-      return HttpResponse.text('Event not found', { status: 404 });
-    }
-    return HttpResponse.json({ ...event, ...form });
-  });
-
-export const deleteEvent = (events: Event[]) =>
-  http.delete('/api/events/:id', ({ params }) => {
-    const { id } = params;
-    const eventIndex = events.findIndex((event) => event.id === id);
-    if (eventIndex === -1) {
-      return HttpResponse.text('Event not found', { status: 404 });
-    }
-    events.splice(eventIndex, 1);
-    return HttpResponse.text('Event deleted', { status: 204 });
-  });
