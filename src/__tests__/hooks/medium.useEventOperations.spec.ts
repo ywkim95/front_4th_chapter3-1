6cef1d9b7f0a55eb272398ff7c1541bf91107028
KimYoungWoo <rladuddn3924@gmail.com>
2025-02-04 17:17:20 +0900

    ✅ useEventOperations 2번 테스트 분리 및 통과 작성

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -9,21 +9,70 @@ import {
 import { events } from '../../__mocks__/response/events.json' assert { type: 'json' };
 import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { server } from '../../setupTests.ts';
-import { Event } from '../../types.ts';
+import { Event, EventForm } from '../../types.ts';
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
-  const { result } = renderHook(() => useEventOperations(false));
+let onSave = vi.fn();
 
-  expect(result.current.events).toEqual([]);
-
-  await waitFor(() => {
-    expect(result.current.events).not.toEqual([]);
-  });
-
-  expect(result.current.events).toEqual(events);
+beforeEach(() => {
+  onSave = vi.fn();
 });
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+  setupMockHandlerCreation(events);
+
+  const { result } = renderHook(() => useEventOperations(false));
+
+  await waitFor(() => expect(result.current.events).toEqual(events));
+});
+
+it('새로운 이벤트 정보를 입력하면 이벤트가 저장된다.', async () => {
+  const newEvent: EventForm = {
+    title: '새로운 회의',
+    date: '2024-10-16',
+    startTime: '09:00',
+    endTime: '10:00',
+    description: '새로운 팀 미팅',
+    location: '회의실 A',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  };
+
+  setupMockHandlerCreation(events);
+
+  const { result } = renderHook(() => useEventOperations(false, onSave));
+
+  act(() => {
+    result.current.saveEvent(newEvent);
+  });
+
+  await waitFor(() => expect(onSave).toHaveBeenCalledOnce());
+});
+
+it('기존 이벤트 정보를 기반으로 적절하게 업데이트를 한다.', async () => {
+  const updatedEvent: Event = {
+    id: '1',
+    title: '기존 회의',
+    date: '2024-10-16',
+    startTime: '09:00',
+    endTime: '10:00',
+    description: '기존 팀 미팅',
+    location: '회의실 B',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  };
+
+  setupMockHandlerUpdating();
+
+  const { result } = renderHook(() => useEventOperations(true, onSave));
+
+  act(() => {
+    result.current.saveEvent(updatedEvent);
+  });
+
+  await waitFor(() => expect(onSave).toHaveBeenCalledOnce());
+});
 
 it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
 
