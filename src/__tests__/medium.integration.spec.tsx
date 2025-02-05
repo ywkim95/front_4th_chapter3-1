d0585e4c9b8b1a08c2140186124bda80ca39ad93
KimYoungWoo <rladuddn3924@gmail.com>
2025-02-06 01:58:45 +0900

    ✅ 통합테스트 통과 코드 작성
    - 굉장히 많은 부분이 중복되어 선언되어 있음
    - 이중에서 전혀 바뀌지않고 중복 선언된 값들 또한 존재
    - 심화과제 이후 시간이 된다면 테스트코드 리팩토링 진행 예정

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -3,6 +3,7 @@ import { render, screen, within, act, waitFor, fireEvent } from '@testing-librar
 import { UserEvent, userEvent } from '@testing-library/user-event';
 import { http, HttpResponse } from 'msw';
 import React, { ReactElement } from 'react';
+import { beforeEach } from 'vitest';
 
 import App from '../App';
 import { server } from '../setupTests';
@@ -15,64 +16,49 @@ vi.mock('@chakra-ui/react', async () => {
     useToast: () => mockToast,
   };
 });
+const mockApis = (initEvents: Event[] = []) => {
+  const mockEvents = structuredClone(initEvents) as Event[];
+  server.use(
+    http.get('/api/events', () => HttpResponse.json({ events: mockEvents })),
+    http.post('/api/events', async ({ request }) => {
+      const form = (await request.json()) as EventForm;
+      const newEvent = {
+        id: String(mockEvents.length + 1),
+        ...form,
+      };
+      mockEvents.push(newEvent);
+      return HttpResponse.json(newEvent, { status: 201 });
+    }),
+    http.put('/api/events/:id', async ({ params, request }) => {
+      const { id } = params;
+      const form = (await request.json()) as Event;
+      const event = mockEvents.find((event) => event.id === id);
+      if (!event) {
+        return HttpResponse.json('Event not found', { status: 404 });
+      }
+      mockEvents.splice(
+        mockEvents.findIndex((event) => event.id === id),
+        1,
+        { ...event, ...form },
+      );
+      return HttpResponse.json({ ...event, ...form }, { status: 200 });
+    }),
+    http.delete('/api/events/:id', ({ params }) => {
+      const { id } = params;
+      const eventIndex = mockEvents.findIndex((event) => event.id === id);
+
+      if (eventIndex === -1) {
+        return HttpResponse.json('Event not found', { status: 404 });
+      }
+      mockEvents.splice(eventIndex, 1);
+      return new HttpResponse(null, { status: 204 });
+    }),
+  );
+};
 describe('일정 CRUD 및 기본 기능', () => {
-  const mockApis = () => {
-    const mockEvents = [
-      {
-        id: '1',
-        title: '기존 회의',
-        date: '2025-02-25',
-        startTime: '09:00',
-        endTime: '10:00',
-        description: '기존 팀 미팅',
-        location: '회의실 B',
-        category: '업무',
-        repeat: { type: 'none', interval: 0 },
-        notificationTime: 10,
-      },
-    ] as Event[];
-    server.use(
-      http.get('/api/events', () => HttpResponse.json({ events: mockEvents })),
-      http.post('/api/events', async ({ request }) => {
-        const form = (await request.json()) as EventForm;
-        const newEvent = {
-          id: String(mockEvents.length + 1),
-          ...form,
-        };
-        mockEvents.push(newEvent);
-        return HttpResponse.json(newEvent, { status: 201 });
-      }),
-      http.put('/api/events/:id', async ({ params, request }) => {
-        const { id } = params;
-        const form = (await request.json()) as Event;
-        const event = mockEvents.find((event) => event.id === id);
-        if (!event) {
-          return HttpResponse.json('Event not found', { status: 404 });
-        }
-        mockEvents.splice(
-          mockEvents.findIndex((event) => event.id === id),
-          1,
-          { ...event, ...form },
-        );
-        return HttpResponse.json({ ...event, ...form }, { status: 200 });
-      }),
-      http.delete('/api/events/:id', ({ params }) => {
-        const { id } = params;
-        const eventIndex = mockEvents.findIndex((event) => event.id === id);
-
-        if (eventIndex === -1) {
-          return HttpResponse.json('Event not found', { status: 404 });
-        }
-        mockEvents.splice(eventIndex, 1);
-        return new HttpResponse(null, { status: 204 });
-      }),
-    );
-  };
-
   beforeEach(() => {
-    mockApis();
+    vi.setSystemTime(new Date('2025-02-01'));
   });
-
   const renderApp = () =>
     render(
       <ChakraProvider>
@@ -81,8 +67,9 @@ describe('일정 CRUD 및 기본 기능', () => {
     );
   it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
     // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+    mockApis();
     renderApp();
-    const user = userEvent.setup();
+    const user = userEvent.setup({ delay: null });
 
     // 일정 추가 폼에 일정 정보 입력
     const titleInput = screen.getByLabelText(/제목/i);
@@ -130,8 +117,22 @@ describe('일정 CRUD 및 기본 기능', () => {
   });
 
   it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
     renderApp();
-    const user = userEvent.setup();
+    const user = userEvent.setup({ delay: null });
     const eventList = screen.getByTestId('event-list');
     await waitFor(() => {
       expect(eventList).toHaveTextContent('기존 회의');
@@ -153,8 +154,22 @@ describe('일정 CRUD 및 기본 기능', () => {
   });
 
   it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
     renderApp();
-    const user = userEvent.setup();
+    const user = userEvent.setup({ delay: null });
     const eventList = screen.getByTestId('event-list');
     await waitFor(() => {
       expect(eventList).toHaveTextContent('기존 회의');
@@ -171,29 +186,426 @@ describe('일정 CRUD 및 기본 기능', () => {
 });
 
 describe('일정 뷰', () => {
-  it('주별 뷰를 선택 후 해당 주에 일정이 없으면, 일정이 표시되지 않는다.', async () => {});
+  const renderApp = () =>
+    render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>,
+    );
+  it('주별 뷰를 선택 후 해당 주에 일정이 없으면, 일정이 표시되지 않는다.', async () => {
+    vi.setSystemTime(new Date('2025-02-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
 
-  it('주별 뷰 선택 후 해당 일자에 일정이 존재한다면 해당 일정이 정확히 표시된다', async () => {});
+    const weekViewButton = screen.getByLabelText('view');
+    await user.selectOptions(weekViewButton, 'week');
+    const eventList = screen.getByTestId('event-list');
+    expect(eventList).toHaveTextContent('검색 결과가 없습니다.');
+  });
 
-  it('월별 뷰에 일정이 없으면, 일정이 표시되지 않아야 한다.', async () => {});
+  it('주별 뷰 선택 후 해당 일자에 일정이 존재한다면 해당 일정이 정확히 표시된다', async () => {
+    vi.setSystemTime(new Date('2025-02-24'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
 
-  it('월별 뷰에 일정이 정확히 표시되는지 확인한다', async () => {});
+    const weekViewButton = screen.getByLabelText('view');
+    await user.selectOptions(weekViewButton, 'week');
 
-  it('달력에 1월 1일(신정)이 공휴일로 표시되는지 확인한다', async () => {});
+    const eventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(eventList).toHaveTextContent('기존 회의');
+    });
+  });
+
+  it('월별 뷰에 일정이 없으면, 일정이 표시되지 않아야 한다.', async () => {
+    vi.setSystemTime(new Date('2025-01-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
+
+    const monthViewButton = screen.getByLabelText('view');
+    await user.selectOptions(monthViewButton, 'month');
+    const eventList = screen.getByTestId('event-list');
+    expect(eventList).toHaveTextContent('검색 결과가 없습니다.');
+  });
+
+  it('월별 뷰에 일정이 정확히 표시되는지 확인한다', async () => {
+    vi.setSystemTime(new Date('2025-02-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
+
+    const monthViewButton = screen.getByLabelText('view');
+    await user.selectOptions(monthViewButton, 'month');
+
+    const eventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(eventList).toHaveTextContent('기존 회의');
+    });
+  });
+
+  it('달력에 1월 1일(신정)이 공휴일로 표시되는지 확인한다', async () => {
+    vi.setSystemTime(new Date('2024-01-01'));
+    mockApis();
+    renderApp();
+    const user = userEvent.setup({ delay: null });
+
+    const monthViewButton = screen.getByLabelText('view');
+    await user.selectOptions(monthViewButton, 'month');
+
+    const calendar = screen.getByTestId('month-view');
+    await vi.waitFor(() => {
+      expect(calendar).toHaveTextContent('신정');
+    });
+  });
 });
 
 describe('검색 기능', () => {
-  it('검색 결과가 없으면, "검색 결과가 없습니다."가 표시되어야 한다.', async () => {});
+  const renderApp = () =>
+    render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>,
+    );
 
-  it("'팀 회의'를 검색하면 해당 제목을 가진 일정이 리스트에 노출된다", async () => {});
+  it('검색 결과가 없으면, "검색 결과가 없습니다."가 표시되어야 한다.', async () => {
+    vi.setSystemTime(new Date('2025-02-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
 
-  it('검색어를 지우면 모든 일정이 다시 표시되어야 한다', async () => {});
+    const prevEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(prevEventList).toHaveTextContent('기존 회의');
+    });
+
+    const searchInput = screen.getByLabelText(/검색/i);
+    await user.type(searchInput, 'aaaaaaaaaaaaaaaaa');
+
+    const rerenderEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(rerenderEventList).toHaveTextContent('검색 결과가 없습니다.');
+    });
+  });
+
+  it("'팀 회의'를 검색하면 해당 제목을 가진 일정이 리스트에 노출된다", async () => {
+    vi.setSystemTime(new Date('2025-02-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '팀 회의',
+        date: '2025-02-26',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '팀 미팅',
+        location: '회의실 C',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
+
+    const prevEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(prevEventList).toHaveTextContent('기존 회의');
+    });
+
+    const searchInput = screen.getByLabelText(/검색/i);
+    await user.type(searchInput, '팀 회의');
+
+    const rerenderEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(rerenderEventList).not.toHaveTextContent('기존 회의');
+    });
+  });
+
+  it('검색어를 지우면 모든 일정이 다시 표시되어야 한다', async () => {
+    vi.setSystemTime(new Date('2025-02-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '팀 회의',
+        date: '2025-02-26',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '팀 미팅',
+        location: '회의실 C',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
+
+    const prevEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(prevEventList).toHaveTextContent('기존 회의');
+    });
+
+    const searchInput = screen.getByLabelText(/검색/i);
+    await user.type(searchInput, '팀 회의');
+
+    const rerenderEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(rerenderEventList).not.toHaveTextContent('기존 회의');
+    });
+
+    await user.clear(searchInput);
+
+    const allEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(allEventList).toHaveTextContent('기존 회의');
+    });
+  });
 });
 
 describe('일정 충돌', () => {
-  it('겹치는 시간에 새 일정을 추가할 때 경고가 표시된다', async () => {});
+  const renderApp = () =>
+    render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>,
+    );
+  it('겹치는 시간에 새 일정을 추가할 때 경고가 표시된다', async () => {
+    vi.setSystemTime(new Date('2025-02-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
 
-  it('기존 일정의 시간을 수정하여 충돌이 발생하면 경고가 노출된다', async () => {});
+    const prevEventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(prevEventList).toHaveTextContent('기존 회의');
+    });
+
+    const titleInput = screen.getByLabelText(/제목/i);
+    await user.type(titleInput, '신규 팀 회의');
+
+    const dateInput = screen.getByLabelText(/날짜/i);
+    await user.type(dateInput, '2025-02-25');
+
+    const startTimeInput = screen.getByLabelText(/시작 시간/i);
+    await user.type(startTimeInput, '09:30');
+
+    const endTimeInput = screen.getByLabelText(/종료 시간/i);
+    await user.type(endTimeInput, '10:30');
+
+    const descriptionInput = screen.getByLabelText(/설명/i);
+    await user.type(descriptionInput, '신규 팀 회의');
+
+    const locationInput = screen.getByLabelText(/위치/i);
+    await user.type(locationInput, '회의실 A');
+
+    const categoryInput = screen.getByLabelText(/카테고리/i);
+    await user.selectOptions(categoryInput, '업무');
+
+    const button = screen.getByTestId('event-submit-button');
+    await user.click(button);
+
+    const alert = screen.getByRole('alertdialog');
+    expect(alert).toHaveTextContent('일정 겹침 경고');
+  });
+
+  it('기존 일정의 시간을 수정하여 충돌이 발생하면 경고가 노출된다', async () => {
+    vi.setSystemTime(new Date('2025-02-01'));
+    mockApis([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-02-25',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '팀 회의',
+        date: '2025-02-26',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '팀 미팅',
+        location: '회의실 C',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+    renderApp();
+    const user = userEvent.setup({ delay: null });
+
+    const eventList = screen.getByTestId('event-list');
+    await waitFor(() => {
+      expect(eventList).toHaveTextContent('기존 회의');
+    });
+
+    const editButton = within(eventList).getAllByLabelText('Edit event');
+    await user.click(editButton[0]);
+
+    expect(screen.getByLabelText(/제목/i)).toHaveValue('기존 회의');
+
+    const dateInput = screen.getByLabelText(/날짜/i);
+    await user.clear(dateInput);
+    await user.type(dateInput, '2025-02-26');
+
+    const startTimeInput = screen.getByLabelText(/시작 시간/i);
+    await user.clear(startTimeInput);
+    await user.type(startTimeInput, '09:30');
+
+    const endTimeInput = screen.getByLabelText(/종료 시간/i);
+    await user.clear(endTimeInput);
+    await user.type(endTimeInput, '10:30');
+
+    const button = screen.getByTestId('event-submit-button');
+    await user.click(button);
+
+    await waitFor(() => {
+      const alert = screen.getByRole('alertdialog');
+      expect(alert).toHaveTextContent('일정 겹침 경고');
+    });
+  });
 });
 
-it('notificationTime을 10으로 하면 지정 시간 10분 전 알람 텍스트가 노출된다', async () => {});
+it('notificationTime을 10으로 하면 지정 시간 10분 전 알람 텍스트가 노출된다', async () => {
+  vi.setSystemTime(new Date('2025-02-25T08:50:00'));
+  mockApis([
+    {
+      id: '1',
+      title: '기존 회의',
+      date: '2025-02-25',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+  ]);
+  render(
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>,
+  );
+  const user = userEvent.setup({ delay: null });
+
+  const prevEventList = screen.getByTestId('event-list');
+  await waitFor(() => {
+    expect(prevEventList).toHaveTextContent('기존 회의');
+  });
+
+  await vi.advanceTimersByTime(0);
+  await vi.advanceTimersByTimeAsync(1000);
+
+  const alert = screen.getByRole('alert');
+  expect(alert).toHaveTextContent('10분 후 기존 회의 일정이 시작됩니다.');
+});
